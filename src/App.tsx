e52c03f7a9f462492c8e281833bab3d658c59440
Vlad Vlasenko <kinzazdev579@gmail.com>
2023-09-16 11:34:38 +0300

    base

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Simple } from "./examples/Simple";
+
+function App() {
+  return (
+    <div className="App">
+      <Simple />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/examples/Simple.tsx
@@ -0,0 +1,171 @@
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+
+const items = Array.from({ length: 10000 }, (_, index) => ({
+  id: Math.random().toString(36).slice(2),
+  text: String(index),
+}));
+
+interface UseFixedSizeListProps {
+  itemsCount: number;
+  itemHeight: number;
+  listHeight: number;
+  overscan?: number;
+  scrollingDelay?: number;
+  getScrollElement?: () => HTMLElement | null;
+}
+
+const DEFAULT_OVERSCAN = 3;
+const DEFAULT_SCROLLING_DELAY = 100;
+
+const itemHeight = 40;
+const containerHeight = 600;
+
+function useFixedSizeList({
+  itemHeight,
+  itemsCount,
+  listHeight,
+  getScrollElement,
+  overscan = DEFAULT_OVERSCAN,
+  scrollingDelay = DEFAULT_SCROLLING_DELAY,
+}: UseFixedSizeListProps) {
+  const [scrollTop, setScrollTop] = useState(0);
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useLayoutEffect(() => {
+    const scrollElement = getScrollElement?.();
+    if (!scrollElement) return;
+
+    const handleScroll = () => {
+      const scrollTop = scrollElement.scrollTop;
+      setScrollTop(scrollTop);
+    };
+
+    handleScroll();
+
+    scrollElement.addEventListener("scroll", handleScroll);
+    return () => scrollElement.removeEventListener("scroll", handleScroll);
+  }, [getScrollElement]);
+
+  useEffect(() => {
+    const scrollElement = getScrollElement?.();
+    if (!scrollElement) return;
+
+    let timeoutId: number | null = null;
+
+    const handleScroll = () => {
+      setIsScrolling(true);
+
+      if (typeof timeoutId === "number") {
+        clearTimeout(timeoutId);
+      }
+
+      // @ts-ignore
+      timeoutId = setTimeout(() => {
+        setIsScrolling(false);
+      }, scrollingDelay);
+    };
+
+    scrollElement.addEventListener("scroll", handleScroll);
+    return () => {
+      scrollElement.removeEventListener("scroll", handleScroll);
+      if (typeof timeoutId === "number") {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [getScrollElement, scrollingDelay]);
+
+  const { virtualItems, startIndex, endIndex } = useMemo(() => {
+    const rangeStart = scrollTop;
+    const rangeEnd = scrollTop + listHeight;
+
+    let startIndex = Math.floor(rangeStart / itemHeight);
+    let endIndex = Math.ceil(rangeEnd / itemHeight);
+
+    startIndex = Math.max(0, startIndex - overscan);
+    endIndex = Math.min(itemsCount - 1, endIndex + overscan);
+
+    const virtualItems = [];
+
+    for (let index = startIndex; index <= endIndex; index++) {
+      virtualItems.push({
+        index,
+        offsetTop: index * itemHeight,
+      });
+    }
+
+    return { virtualItems, startIndex, endIndex };
+  }, [itemHeight, itemsCount, listHeight, scrollTop, overscan]);
+
+  const totalHeight = itemHeight * itemsCount;
+
+  return {
+    virtualItems,
+    totalHeight,
+    startIndex,
+    endIndex,
+    isScrolling,
+  };
+}
+
+export const Simple = () => {
+  const [listItems, setListItems] = useState(items);
+  const scrollElementRef = useRef<HTMLDivElement>(null);
+
+  const { isScrolling, totalHeight, virtualItems } = useFixedSizeList({
+    itemHeight: itemHeight,
+    itemsCount: listItems.length,
+    listHeight: containerHeight,
+    getScrollElement: useCallback(() => scrollElementRef.current, []),
+  });
+
+  console.log("isScrolling:", isScrolling);
+
+  return (
+    <div style={{ padding: "0 12px" }}>
+      <h1>List</h1>
+      <div style={{ marginBottom: 12 }}>
+        <button
+          onClick={() => setListItems((items) => items.slice().reverse())}
+        >
+          reverse
+        </button>
+      </div>
+      <div
+        ref={scrollElementRef}
+        style={{
+          height: containerHeight,
+          overflow: "auto",
+          border: "1px solid lightgrey",
+          position: "relative",
+        }}
+      >
+        <div style={{ height: totalHeight }}>
+          {virtualItems.map((virtualItem) => {
+            const item = listItems[virtualItem.index];
+            return (
+              <div
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  transform: `translateY(${virtualItem.offsetTop}px)`,
+                  height: itemHeight,
+                  padding: "6px 12px",
+                }}
+                key={item.id}
+              >
+                {item.text}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
